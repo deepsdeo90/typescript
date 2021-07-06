52f0b591435d5166c71a8b1dca6733e6fa4dfd60
Dipali <deeps0310@gmail.com>
2021-07-06 15:58:53 -0700

    render form and basic design

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello there");
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        this.templateElement = document.getElementById("project");
+        this.hostElement = document.getElementById("app");
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    ProjectInput.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    };
+    return ProjectInput;
+}());
+var projectInput = new ProjectInput();
--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-console.log("hello there");
+class ProjectInput {
+    templateElement : HTMLTemplateElement;
+    hostElement : HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor(){
+        this.templateElement = document.getElementById("project")! as HTMLTemplateElement;
+        this.hostElement = document.getElementById("app")! as HTMLDivElement;
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+    private attach(){
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const projectInput = new ProjectInput();
